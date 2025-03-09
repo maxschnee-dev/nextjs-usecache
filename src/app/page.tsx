3b426d188534f8cc256e7da523f7ed897569d5da
Max <maxschnee.dev@gmail.com>
2025-03-09 15:41:08 +0100

    initrim

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
-import Image from "next/image";
+import { unstable_cacheTag as cacheTag, revalidateTag } from "next/cache";
 
-export default function Home() {
+export default async function Home() {
+  "use cache";
+  cacheTag("homepage");
+
+  const date = new Date().toISOString();
+
   return (
-    <div className="flex justify-center items-center h-screen">
-      Test the new nextjs canary bullshit features!@!@!{">"}!{">"}!
-    </div>
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <h1>{date}</h1>
+        <form
+          action={async () => {
+            "use server";
+            revalidateTag("homepage");
+          }}
+        >
+          <button type="submit">Reveal</button>
+        </form>
+      </div>
+    </>
   );
 }
